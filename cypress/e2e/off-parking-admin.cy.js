2691eb56412fdfa5d482fa11644b2cf48e94f8f6
Low Seah Guan <seahguanlow@gmail.com>
2024-03-31 11:10:04 +0800

    Initail commit from Mac

---
+++ cypress/e2e/off-parking-admin.cy.js
@@ -0,0 +1,18 @@
+describe('example to-do app', () => {
+    beforeEach(() => {
+      // open the setel admin dashboard
+    //   cy.visit('/')
+    })
+
+    it('Login to Setel Admin', () => {
+        // Login to admin portal
+        cy.visit('/')
+    
+        // Now that we've typed our new item, let's check that it actually was added to the list.
+        // Since it's the newest item, it should exist as the last element in the list.
+        // In addition, with the two default items, we should have a total of 3 elements in the list.
+        // Since assertions yield the element that was asserted on,
+        // we can chain both of these assertions together into a single statement.
+        
+      })
+})
